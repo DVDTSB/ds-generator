b6349781fac2608b9cd08a2d8ad1af7f9c166b68
DVDTSB <david15tache@gmail.com>
2024-02-27 23:49:09 +0200

    smol

--- genericgenerator.js
+++ genericgenerator.js
@@ -273,7 +273,7 @@ function generate() {
 //generate 1200 names and add them to a file data2.txt
 const fs = require("fs");
 let data = [];
-for (let i = 0; i < 2000; i++) {
+for (let i = 0; i < 100; i++) {
   data.push(generate());
 }
 fs.writeFileSync("data2.txt", data.join("\n"));
